483f0343a72d76d2e3f877fc454a49c42fd60a1b
Cristián Rojas <injcristianrojas@gmail.com>
2017-03-14 20:34:35 -0300

    A counter, for a start.

--- extension.js
+++ extension.js
@@ -1,8 +1,11 @@
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const GnomeDesktop = imports.gi.GnomeDesktop;
+const Lang = imports.lang;
 
-let text, button;
+let text, button, label;
+let counter = 0;
 
 function init() {
     button = new St.Bin({ style_class: 'panel-button',
@@ -11,15 +14,26 @@ function init() {
                           x_fill: true,
                           y_fill: false,
                           track_hover: true });
-    let label = new St.Label({text: '00:00 UTC', opacity: 200});
+    label = new St.Label({text: '00:00 UTC', opacity: 200});
 
     button.set_child(label);
 }
 
 function enable() {
     Main.panel._centerBox.insert_child_at_index(button, 1);
+    clock_this();
 }
 
 function disable() {
     Main.panel._centerBox.remove_child(button);
+}
+
+function clock_this() {
+    let clock = new GnomeDesktop.WallClock();
+    let clock_signal_id = clock.connect('notify::clock', Lang.bind(this, this.update_time));
+}
+
+function update_time() {
+    counter++;
+    label.set_text('' + counter);
 }
