ce1c5babdd1de1899c58cffd0655832b1e687d4c
Cristián Rojas <injcristianrojas@gmail.com>
2017-03-14 20:13:03 -0300

    Removed Hello World code

--- extension.js
+++ extension.js
@@ -1,34 +1,8 @@
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
 
 let text, button;
-
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
-
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
 
 function init() {
     button = new St.Bin({ style_class: 'panel-button',
@@ -40,7 +14,6 @@ function init() {
     let label = new St.Label({text: '00:00 UTC', opacity: 200});
 
     button.set_child(label);
-    button.connect('button-press-event', _showHello);
 }
 
 function enable() {
