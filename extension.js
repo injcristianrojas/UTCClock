ac5881cce640b7853e0a940bd3d15960bf6e281f
Cristian Rojas <injcristianrojas@gmail.com>
2021-04-04 19:04:59 -0400

    Working clock

--- extension.js
+++ extension.js
@@ -1,150 +1,87 @@
 "use strict";
 
-const St = imports.gi.St;
-const Main = imports.ui.main;
+const Clutter = imports.gi.Clutter;
 const GnomeDesktop = imports.gi.GnomeDesktop;
+const GObject = imports.gi.GObject;
 const Lang = imports.lang;
-const Shell = imports.gi.Shell;
-const Util = imports.misc.util;
-const Config = imports.misc.config;
-
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const Convenience = Me.imports.convenience;
-
-const log_this = Convenience.log_this;
-const version_data = Convenience.version_data;
-const getClockSecondsSettings = Convenience.getClockSecondsSettings;
-const isGnome40 = Convenience.isGnome40;
-
-let text, button, label;
-let clock, clock_signal_id;
-let settings, seconds_settings;
-
-let signals = [];
-
-let shellMinorVersion36 = parseInt(version_data[1]) < 36;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const St = imports.gi.St;
 
 let format_params = {
     hour: "2-digit",
     minute: "2-digit",
     hour12: false
 }
+
 let time_text = "UTC";
 
+let UTCClock = GObject.registerClass(
+    class UTCCLock extends PanelMenu.Button {
+
+        _init() {
+            super._init(0, 'UTCClock', false);
+
+            // Label
+            this._timeText = new St.Label({
+                y_align: Clutter.ActorAlign.CENTER,
+                text: "..."
+            });
+
+            let topBox = new St.BoxLayout();
+            topBox.add_actor(this._timeText);
+            this.add_actor(topBox);
+
+            this.enable();
+        }
+
+        update_time() {
+            let now = new Date();
+            let utc = new Date(
+                now.getUTCFullYear(),
+                now.getUTCMonth(),
+                now.getUTCDate(),
+                now.getUTCHours(),
+                now.getUTCMinutes(),
+                now.getUTCSeconds()
+            );
+            this._timeText.set_text(
+                new Intl.DateTimeFormat(
+                    "default", format_params
+                ).format(utc) + " " + time_text
+            );
+        }
+
+        enable() {
+            this.signals = [];
+            this.clock = new GnomeDesktop.WallClock();
+            this.signals[0] = this.clock.connect(
+                "notify::clock",
+                Lang.bind(this, this.update_time)
+            );
+            
+        }
+
+        disable() {
+            this.clock.disconnect(this.signals[0]);
+        }
+
+
+    }
+);
+
+let utcclock;
+
 function init() {
-    settings = Convenience.getSettings();
-    seconds_settings = Convenience.getSettings("org.gnome.desktop.interface");
 
-    clock = new GnomeDesktop.WallClock();
-    button = new St.Bin({
-        style_class: "panel-button",
-        reactive: true,
-        can_focus: false,
-        x_expand: true,
-        y_expand: false,
-        y_align: !isGnome40 && shellMinorVersion36 ? St.Align.MIDDLE : St.Align.END,
-        track_hover: !isGnome40
-    });
-
-    label = new St.Label({
-        opacity: 200
-    });
-    
-    button.set_child(label);
 }
 
 function enable() {
-    log_this("enabling...");
-
-    signals[0] = settings.connect("changed::show-seconds", Lang.bind(this, setSecondsDisplayed));
-    signals[1] = settings.connect("changed::time-text", Lang.bind(this, setTimeText));
-    signals[2] = settings.connect("changed::show-date", Lang.bind(this, setDateDisplayed));
-    signals[3] = settings.connect("changed::light-opacity", Lang.bind(this, setLightOpacity));
-    
-    signals[4] = button.connect("button-press-event", showMenu);
-    
-    setSecondsDisplayed();
-    setTimeText();
-    setDateDisplayed();
-    setLightOpacity();
-    
-    update_time();
-    
-    signals[5] = clock.connect("notify::clock", Lang.bind(this, this.update_time));
-    
-    Main.panel._centerBox.insert_child_at_index(button, 1);
-    
-    signals[6] = seconds_settings.connect("changed::clock-show-seconds", Lang.bind(this, setGNOMESecondsEnabled));
-    
-    log_this("enabled.");
+    utcclock = new UTCClock();
+    Main.panel._addToPanelBox('utcclock', utcclock, 1, Main.panel._centerBox);
 }
 
 function disable() {
-    log_this("disabling...");
-
-    settings.disconnect(signals[0]);
-    settings.disconnect(signals[1]);
-    settings.disconnect(signals[2]);
-    settings.disconnect(signals[3]);
-    button.disconnect(signals[4]);
-
-    Main.panel._centerBox.remove_child(button);
-    clock.disconnect(signals[5]);
-    log_this("disabled.");
-}
-
-function update_time() {
-    var now = new Date();
-    var utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
-    label.set_text(new Intl.DateTimeFormat("default", format_params).format(utc) + " " + time_text);
-}
-
-function setSecondsDisplayed() {
-    let secondsDisplayed = settings.get_boolean("show-seconds");
-    if (secondsDisplayed) {
-        format_params["second"] = "2-digit";
-    } else {
-        delete format_params["second"];
-    }
-    update_time();
-}
-
-function setTimeText() {
-    time_text = settings.get_string("time-text");
-    update_time();
-}
-
-function setDateDisplayed() {
-    let dateDisplayed = settings.get_boolean("show-date");
-    if (dateDisplayed) {
-        format_params["weekday"] = "short";
-        format_params["month"] = "short";
-        format_params["day"] = "numeric";
-    } else {
-        delete format_params["weekday"];
-        delete format_params["month"];
-        delete format_params["day"];
-    }
-    update_time();
-}
-
-function setLightOpacity() {
-    label.opacity = settings.get_boolean("light-opacity") ? 255 : 200;
-    update_time();
-}
-
-function showMenu() {
-    Util.spawn([
-        "gnome-shell-extension-prefs",
-        Me.uuid
-    ]);
-}
-
-function setGNOMESecondsEnabled() {
-    settings.set_boolean(
-        "show-seconds",
-        seconds_settings.get_boolean("clock-show-seconds") &&
-        settings.get_boolean("show-seconds")
-    );
+    utcclock.disable();
+    utcclock.destroy();
 }
