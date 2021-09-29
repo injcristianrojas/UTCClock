7e78c15f2892220624b6bb40f6edee52ba4a23e6
Cristian Rojas <injcristianrojas@gmail.com>
2021-09-29 17:38:31 -0300

    Removed double quotes and added logger

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const Clutter = imports.gi.Clutter;
 const ExtensionUtils = imports.misc.extensionUtils;
@@ -13,17 +13,18 @@ const PopupMenu = imports.ui.popupMenu;
 const St = imports.gi.St;
 
 const Convenience = Me.imports.convenience;
+const log_this = Convenience.log_this;
 
 let UTCClock = GObject.registerClass(
     class UTCCLock extends PanelMenu.Button {
 
         _init() {
-            super._init(0, "UTCClock", false);
+            super._init(0, 'UTCClock', false);
 
             // Label
             this.timeText = new St.Label({
                 y_align: Clutter.ActorAlign.CENTER,
-                text: "..."
+                text: '...'
             });
 
             let topBox = new St.BoxLayout();
@@ -45,116 +46,116 @@ let UTCClock = GObject.registerClass(
             );
             this.timeText.set_text(
                 new Intl.DateTimeFormat(
-                    "default", this.format_params
-                ).format(utc) + " " + this.time_text
+                    'default', this.format_params
+                ).format(utc) + ' ' + this.time_text
             );
         }
 
         setSecondsDisplayed() {
-            let secondsDisplayed = this.settings.get_boolean("show-seconds");
+            let secondsDisplayed = this.settings.get_boolean('show-seconds');
             if (secondsDisplayed) {
-                this.format_params["second"] = "2-digit";
+                this.format_params['second'] = '2-digit';
             } else {
-                delete this.format_params["second"];
+                delete this.format_params['second'];
             }
             this.updateTime();
         }
 
         setTimeText() {
-            this.time_text = this.settings.get_string("time-text");
+            this.time_text = this.settings.get_string('time-text');
             this.updateTime();
         }
 
         setDateDisplayed() {
-            let dateDisplayed = this.settings.get_boolean("show-date");
+            let dateDisplayed = this.settings.get_boolean('show-date');
             if (dateDisplayed) {
-                this.format_params["weekday"] = "short";
-                this.format_params["month"] = "short";
-                this.format_params["day"] = "numeric";
+                this.format_params['weekday'] = 'short';
+                this.format_params['month'] = 'short';
+                this.format_params['day'] = 'numeric';
             } else {
-                delete this.format_params["weekday"];
-                delete this.format_params["month"];
-                delete this.format_params["day"];
+                delete this.format_params['weekday'];
+                delete this.format_params['month'];
+                delete this.format_params['day'];
             }
             this.updateTime();
         }
 
         setLightOpacity() {
             this.timeText.opacity =
-                this.settings.get_boolean("light-opacity") ? 200 : 255;
+                this.settings.get_boolean('light-opacity') ? 200 : 255;
             this.updateTime();
         }
 
         buildMenu() {
             this.ClockMenuItemSeconds = new PopupMenu.PopupSwitchMenuItem(
-                "Show seconds",
-                this.settings.get_boolean("show-seconds"),
+                'Show seconds',
+                this.settings.get_boolean('show-seconds'),
                 { reactive: true }
             );
             this.menuSignal1 = this.ClockMenuItemSeconds.connect(
-                "toggled",
+                'toggled',
                 Lang.bind(
                     this, function(object, value) {
-                        this.settings.set_boolean("show-seconds", value);
+                        this.settings.set_boolean('show-seconds', value);
                     }
                 )
             );
             this.menu.addMenuItem(this.ClockMenuItemSeconds);
 
             this.ClockMenuItemText = new PopupMenu.PopupSubMenuMenuItem(
-                "Time text to show"
+                'Time text to show'
             );
-            this.PopupMenuItemUTC = new PopupMenu.PopupMenuItem("UTC");
+            this.PopupMenuItemUTC = new PopupMenu.PopupMenuItem('UTC');
             this.menuSignal2 = this.PopupMenuItemUTC.connect('activate', () => {
-                this.settings.set_string("time-text", "UTC");
+                this.settings.set_string('time-text', 'UTC');
             });
             this.ClockMenuItemText.menu.addMenuItem(this.PopupMenuItemUTC);
-            this.PopupMenuItemGMT = new PopupMenu.PopupMenuItem("GMT");
+            this.PopupMenuItemGMT = new PopupMenu.PopupMenuItem('GMT');
             this.menuSignal3 = this.PopupMenuItemGMT.connect('activate', () => {
-                this.settings.set_string("time-text", "GMT");
+                this.settings.set_string('time-text', 'GMT');
             });
             this.ClockMenuItemText.menu.addMenuItem(this.PopupMenuItemGMT);
-            this.PopupMenuItemZ = new PopupMenu.PopupMenuItem("Z");
+            this.PopupMenuItemZ = new PopupMenu.PopupMenuItem('Z');
             this.menuSignal4 = this.PopupMenuItemZ.connect('activate', () => {
-                this.settings.set_string("time-text", "Z");
+                this.settings.set_string('time-text', 'Z');
             });
             this.ClockMenuItemText.menu.addMenuItem(this.PopupMenuItemZ);
             this.menu.addMenuItem(this.ClockMenuItemText);
 
             this.ClockMenuItemDate = new PopupMenu.PopupSwitchMenuItem(
-                "Show date",
-                this.settings.get_boolean("show-date"),
+                'Show date',
+                this.settings.get_boolean('show-date'),
                 { reactive: true }
             );
             this.menuSignal5 = this.ClockMenuItemDate.connect(
-                "toggled",
+                'toggled',
                 Lang.bind(
                     this, function(object, value) {
-                        this.settings.set_boolean("show-date", value);
+                        this.settings.set_boolean('show-date', value);
                     }
                 )
             );
             this.menu.addMenuItem(this.ClockMenuItemDate);
 
             this.ClockMenuItemOpacity = new PopupMenu.PopupSwitchMenuItem(
-                "Light opacity",
-                this.settings.get_boolean("light-opacity"),
+                'Light opacity',
+                this.settings.get_boolean('light-opacity'),
                 { reactive: true }
             );
             this.menuSignal6 = this.ClockMenuItemOpacity.connect(
-                "toggled",
+                'toggled',
                 Lang.bind(
                     this, function(object, value) {
-                        this.settings.set_boolean("light-opacity", value);
+                        this.settings.set_boolean('light-opacity', value);
                     }
                 )
             );
             this.menu.addMenuItem(this.ClockMenuItemOpacity);
             this.menuSignal7 = this.connect(
-                "button-press-event",
+                'button-press-event',
                 Lang.bind(
                     this, function() {
-                        if (this.gnomeSecondsSettings.get_boolean("clock-show-seconds"))
+                        if (this.gnomeSecondsSettings.get_boolean('clock-show-seconds'))
                             this.ClockMenuItemSeconds.set_reactive(true);
                         else {
                             this.ClockMenuItemSeconds._switch.state = false;
@@ -166,24 +167,24 @@ let UTCClock = GObject.registerClass(
         }
 
         enable() {
-            this.time_text = "UTC";
+            this.time_text = 'UTC';
 
             this.format_params = {
-                hour: "2-digit",
-                minute: "2-digit",
+                hour: '2-digit',
+                minute: '2-digit',
                 hour12: false
             }
 
             this.settings = Convenience.getSettings();
             this.gnomeSecondsSettings = Convenience.getSettings(
-                "org.gnome.desktop.interface"
+                'org.gnome.desktop.interface'
             );
 
             this.gnomeSecondsSignal =  this.gnomeSecondsSettings.connect(
-                "changed::clock-show-seconds",
+                'changed::clock-show-seconds',
                 Lang.bind(this, function() {
-                    if (!this.gnomeSecondsSettings.get_boolean("clock-show-seconds")) {
-                        this.settings.set_boolean("show-seconds", false);
+                    if (!this.gnomeSecondsSettings.get_boolean('clock-show-seconds')) {
+                        this.settings.set_boolean('show-seconds', false);
                         this.setSecondsDisplayed();
                     }
                 })
@@ -193,32 +194,32 @@ let UTCClock = GObject.registerClass(
 
             this.clock = new GnomeDesktop.WallClock();
             this.clockSignal = this.clock.connect(
-                "notify::clock",
+                'notify::clock',
                 this.updateTime.bind(this)
             );
 
-            if (!this.gnomeSecondsSettings.get_boolean("clock-show-seconds"))
-                this.settings.set_boolean("show-seconds", false);
+            if (!this.gnomeSecondsSettings.get_boolean('clock-show-seconds'))
+                this.settings.set_boolean('show-seconds', false);
             this.setSecondsDisplayed();
             this.settingsSignals[0] = this.settings.connect(
-                "changed::show-seconds",
+                'changed::show-seconds',
                 Lang.bind(this, this.setSecondsDisplayed)
             );
 
             this.setTimeText();
-            this.settingsSignals[1] = this.settings.connect("changed::time-text",
+            this.settingsSignals[1] = this.settings.connect('changed::time-text',
                 this.setTimeText.bind(this)
             );
 
             this.setDateDisplayed();
             this.settingsSignals[2] = this.settings.connect(
-                "changed::show-date",
+                'changed::show-date',
                 Lang.bind(this, this.setDateDisplayed)
             );
 
             this.setLightOpacity();
             this.settingsSignals[3] = this.settings.connect(
-                "changed::light-opacity",
+                'changed::light-opacity',
                 Lang.bind(this, this.setLightOpacity)
             );
 
@@ -251,7 +252,7 @@ function init() {
 
 function enable() {
     utcclock = new UTCClock();
-    Main.panel._addToPanelBox("utcclock", utcclock, 1, Main.panel._centerBox);
+    Main.panel._addToPanelBox('utcclock', utcclock, 1, Main.panel._centerBox);
 }
 
 function disable() {
